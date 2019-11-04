5151d88dec1dbc250aec6af8f0bc6e5933695097
Michael Parker <parkervcp@gmail.com>
2019-11-03 22:38:58 -0500

    add notes for the unpacker script.

--- scripts/file_unpacker.sh
+++ scripts/file_unpacker.sh
@@ -1,6 +1,7 @@
 ## only needs to have the file with a file passed into it to get the filetype.
 ## only supports tar.gz/xz and zip currently for unpacking.
-## The value for ${DOWNLOAD_LINK##*/} is 
+## The value for ${DOWNLOAD_LINK##*/} is derived from DOWNLOAD_LINK
+## if the download link is https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar then the ##*/ is server.jar
 
 FILETYPE=$(file -F ',' ${DOWNLOAD_LINK##*/} | cut -d',' -f2 | cut -d' ' -f2)
 if [ "$FILETYPE" == "gzip" ]; then
