5f0855ef3cf957e322cbce829f33df9ae58b2d10
TheRealGramdalf <79593869+TheRealGramdalf@users.noreply.github.com>
2022-08-30 15:01:45 -0700

    fixed unexpected escape character

--- game_eggs/minecraft/crossplay/purpur-geysermc-floodgate/geyserpart.sh
+++ game_eggs/minecraft/crossplay/purpur-geysermc-floodgate/geyserpart.sh
@@ -1,2 +1,12 @@
-\r\nif [ \"$GEYSER_BUILD\" == \"latest-stable\" ]; then\r\n\tDOWNLOAD_URL=https:\/\/ci.opencollab.dev\/job\/GeyserMC\/job\/Floodgate\/job\/master\/lastStableBuild\/artifact\/spigot\/build\/libs\/Geyser-Spigot.jar\r\nelif [ \"$GEYSER_BUILD\" == "latest-successful" ]; then\r\n\tDOWNLOAD_URL=https:\/\/ci.opencollab.dev\/job\/GeyserMC\/job\/Floodgate\/job\/master\/lastSuccessfulBuild\/artifact\/spigot\/build\/libs\/Geyser-Spigot.jar\r\nfi\r\nGEYSER_STABLE_OR_SUCCESSFUL=`echo \"${GEYSER_BUILD}\" | grep -o -e stable -e succsessful`\r\n
-\r\necho -e \'Downloading latest \"${GEYSER_STABLE_OR_SUCCESSFUL}\" build with curl -o \"${DOWNLOAD_URL}\"\'\r\ncurl -o \"${DOWNLOAD_URL}\"\r\n
+
+if [ \"$GEYSER_BUILD\" == \"latest-stable\" ]; then
+    DOWNLOAD_URL=https:\/\/ci.opencollab.dev\/job\/GeyserMC\/job\/Floodgate\/job\/master\/lastStableBuild\/artifact\/spigot\/build\/libs\/Geyser-Spigot.jar
+elif [ \"$GEYSER_BUILD\" == \"latest-successful\" ]; then
+    DOWNLOAD_URL=https:\/\/ci.opencollab.dev\/job\/GeyserMC\/job\/Floodgate\/job\/master\/lastSuccessfulBuild\/artifact\/spigot\/build\/libs\/Geyser-Spigot.jar
+else\r\n\tDOWNLOAD_URL=https:\/\/ci.opencollab.dev\/job\/GeyserMC\/job\/Floodgate\/job\/master\/lastStableBuild\/artifact\/spigot\/build\/libs\/floodgate-spigot.jar
+fi
+GEYSER_STABLE_OR_SUCCESSFUL=`echo \"${GEYSER_BUILD}\" | grep -o -e stable -e succsessful`
+
+
+echo -e \'Downloading latest ${GEYSER_STABLE_OR_SUCCESSFUL} build with curl -o ${DOWNLOAD_URL}\'
+curl -o \"${DOWNLOAD_URL}\"
